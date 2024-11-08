88af9cceeb0955bc5f25dcfc2ccf25e300a31af5
denyskoech <dennis96ist@gmail.com>
2024-11-08 09:13:57 +0200

    the bankist app
    the bankist app

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,18 @@
+/*let js = "amazing";
+
+let myCountry = "Kenya";
+let myContinent = "Africa";
+let myCountryPopulation = 50000000;
+
+console.log(myCountry);
+console.log(myContinent);
+console.log(myCountryPopulation);
+*/
+
+//operators
+const now = 2047; // good idea to declare 2047 as a variable to reuse
+const ageUser = now - 1991;
+const ageClient = now - 2024;
+console.log(ageUser, ageClient);
+
+console.log(ageClient * 2, ageUser / 10, 2 ** 3);
